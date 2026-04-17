70a2de32b3a8fceff4ff51ea348bd52a2e80c198
Le Phu Son <24521528@gm.uit.edu.vn>
2026-04-17 23:44:21 +0700

    them code do rain sensor

--- src/Node.cpp
+++ src/Node.cpp
@@ -5,10 +5,12 @@
 #include <BH1750.h>
 
 Adafruit_BMP280 bmp;
+const int RainSensorPin = A0;
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(RainSensorPin, INPUT);
   if (!bmp.begin(0x76))
   {
     Serial.println("Khong tim thay cam bien BMP280!");
@@ -18,9 +20,12 @@ void setup()
 
 void loop()
 {
+  int RainSensor_Value = analogRead(RainSensorPin);
   Serial.println(bmp.readTemperature()); //đo nhiệt độ
-
+  
   Serial.println(bmp.readPressure()); //đo áp suất
   
+  Serial.println(RainSensor_Value); //đo độ ướt (mưa)
+
   delay(2000);
 }
