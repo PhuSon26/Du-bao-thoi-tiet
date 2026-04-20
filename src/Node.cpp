409303821ecb6f271fbc583d1805a67af19e2b79
Le Phu Son <24521528@gm.uit.edu.vn>
2026-04-20 23:46:47 +0700

    chinh sua dinh dang rainsensor

--- src/Node.cpp
+++ src/Node.cpp
@@ -5,41 +5,18 @@
 #include <BH1750.h>
 #include <Wire.h> //thư viện i2c vì bh1750 truyền theo giao thức này
 
-<<<<<<< HEAD
-Adafruit_BMP280 bmp;
-const int RainSensorPin = A0;
-
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(RainSensorPin, INPUT);
-  if (!bmp.begin(0x76))
-  {
-    Serial.println("Khong tim thay cam bien BMP280!");
-    while (1);
-  }
-}
-
-void loop()
-{
-  int RainSensor_Value = analogRead(RainSensorPin);
-  Serial.println(bmp.readTemperature()); //đo nhiệt độ
-  
-  Serial.println(bmp.readPressure()); //đo áp suất
-  
-  Serial.println(RainSensor_Value); //đo độ ướt (mưa)
-
-  delay(2000);
-}
-=======
 #define DHTTYPE DHT22 // khai báo loại cảm biến là DHT22
-#define DHTPIN 23 // Chân Data của DHT22 nối với GPIO 4
+#define DHTPIN 23 // Chân Data của DHT22 nối với GPIO 23
+
 DHT dht(DHTPIN, DHTTYPE); //khởi tạo object dht gòm có DHTPIN VÀ DHTTYPE
 BH1750 lightMeter; //khởi tạo cảm biến ánh sáng lightMeter
+Adafruit_BMP280 bmp;
+const int RainSensorPin = A0; //analog doc gia tri do uot cua RainSensor
 //----cấu hình LoRa UART (serial2)
 #define RXD2 16
 #define TXD2 17
 #define LoRa_baudrate 9600
+#define threshhold 102 //nho hon gia tri nay se co mua (0-1023)
 void setup() {
   Serial.begin(115200);
   //Serial2.begin(baudrate, config, RX_pin, TX_pin);
@@ -47,21 +24,29 @@ void setup() {
   // config: cấu hình khu dữ liệu. 8: 8 bit dữ liệu, N: không kiểm tra lỗi, 1: bit stop
   //RX_pin: esp32 nhận dữ liệu từ lora
   //TX-pin: esp32 gửi dữ liệu sang lora
+  pinMode(RainSensorPin, INPUT);
+  if (!bmp.begin(0x76))
+  {
+    Serial.println("Khong tim thay cam bien BMP280!");
+    while (1);
+  }
   Serial2.begin(LoRa_baudrate,  SERIAL_8N1, RXD2, TXD2);
   Serial.println("\n--- Khoi dong Tram Phat---");
   Wire.begin(); //Khoi dong I2C
   dht.begin(); //DHT22
+  bmp.begin(); //BMP280
   lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE); // Khoi dong DH1750 o che do do lien tuc (continuous) va do chinh xac cao (high_res)
-
 }
 //Temp,pressure,humid,lux,rain
 //Node gửi qua gateway theo định dạng string này nha
-//Ví dụ: 36,36.36,36.36,36,1
-//Rain thì 0 nếu k mưa, 1 nếu mưa
+//Ví dụ: 36,36.36,36.36,36,36
 void loop() {
+  //float temp = bmp.readTemperature();
+  float temp = dht.readTemperature(); // do nhiet do
+  float pressure = bmp.readPressure(); // do ap suat
   float humid = dht.readHumidity(); // đo độ ẩm không khí
-  float temp = dht.readTemperature(); 
   uint16_t lux = lightMeter.readLightLevel();//vì ánh sáng ko âm nên uint16_t là số nguyên ko âm 16 bit
+  int rain = analogRead(RainSensorPin);
   if(isnan(humid) || isnan(temp)) //isnan(): kieemr tra gia tri bi loi
   {
     Serial.println("Loi doc DHT22!");
@@ -76,4 +61,3 @@ void loop() {
   }
   delay(5000); // chờ 5 s rồi gửi gói tiếp theo
 }
->>>>>>> DHT22_BH1750
